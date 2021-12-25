1808864f369828794b847fbd7e22ec0e2b8d173c
parvejmia9 <61575111+parvejmia9@users.noreply.github.com>
2021-12-25 10:23:28 +0600

    Create dijkstra.cpp

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,23 @@
+vector<pair<ll, ll>> graph[MAXN];
+ll dis[MAXN];
+int par[MAXN];
+void dijkstra(int src) {
+	dis[src] = 0;
+	par[src] = 0;
+	priority_queue<pair<ll, ll>>pq;
+	pq.push({0, src});
+	while (!pq.empty()) {
+		ll node, w;
+		tie(w, node) = pq.top();
+		pq.pop();
+		w *= -1;
+		if (dis[node] < w) continue;
+		for (auto p : graph[node]) {
+			if (dis[p.ff] > dis[node] + p.ss) {
+				dis[p.ff] = dis[node] + p.ss;
+				par[p.ff] = node;
+				pq.push({ -dis[p.ff], p.ff});
+			}
+		}
+	}
+}
