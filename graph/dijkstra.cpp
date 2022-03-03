00ddb2456e575985644ab90523808cc61349aaaa
parvejmia9 <61575111+parvejmia9@users.noreply.github.com>
2022-03-03 07:01:26 +0600

    Update dijkstra.cpp

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -3,22 +3,28 @@
 vector<pair<ll, ll>> graph[MAXN];
 ll dis[MAXN];
 int par[MAXN];
-void dijkstra(int src) {
+void dijkstra(int src,int n) {
 	dis[src] = 0;
 	par[src] = 0;
+	ll dis[n];
+	for (int i = 0; i < n; i++) {
+		dis[i] = 1e18;
+	}
+	dis[0] = 0;
 	priority_queue<pair<ll, ll>>pq;
-	pq.push({0, src});
+	pq.push({0, 0});
 	while (!pq.empty()) {
-		ll node, w;
-		tie(w, node) = pq.top();
+		auto p = pq.top();
 		pq.pop();
-		w *= -1;
-		if (dis[node] < w) continue;
-		for (auto p : graph[node]) {
-			if (dis[p.ff] > dis[node] + p.ss) {
-				dis[p.ff] = dis[node] + p.ss;
-				par[p.ff] = node;
-				pq.push({ -dis[p.ff], p.ff});
+		ll w = -p.ff;
+		ll x = p.ss;
+		if (dis[x] < w) continue;
+		for (auto pp : graph[x]) {
+			int y = pp.ff;
+			if (dis[y] > w + pp.ss) {
+				dis[y] = w + pp.ss;
+				par[y]=x;
+				pq.push({ -dis[y], y});
 			}
 		}
 	}
