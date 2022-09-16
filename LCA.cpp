6c597e48538b5cd38abc4edd750bb4ed93c2c4a4
parvejmia9 <61575111+parvejmia9@users.noreply.github.com>
2022-09-16 12:58:17 +0600

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,54 +1,82 @@
-//1 based
-vector<int> graph[MAXN];
-int par[MAXN];
-int lev[MAXN];
-bool vis[MAXN];
-int sp[MAXN][20];
-int dis[MAXN];
-void dfs(int node, int l , int p) { // finding parents and levels
-	vis[node] = 1;
-	par[node] = p;
-	lev[node] = l;
-	for (int i = 0; i < graph[node].size(); i++) {
-		int x = graph[node][i];
-		if (!vis[x]) {
-			dfs(x, l + 1, node);
-		}
-	}
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5+7, K = 20;
+vector<int> adj[N];
+
+int anc[N][K];
+int level[N];
+
+void setup(int u, int par) {
+    level[u] = level[par]+1;
+    anc[u][0] = par;
+    for (int k=1; k<K; k++)     anc[u][k] = anc[anc[u][k-1]][k-1];
+
+    for (int v: adj[u]) {
+        if (v == par)   continue;
+        setup(v, u);
+    }
 }
-void sparsh(int n) { // creating sparsh table
-	for (int i = 0; i < MAXN; i++) {
-		for (int j = 0; j < 20; j++) {
-			sp[i][j] = -1;
-		}
-	}
-	for (int i = 1; i <= n; i++) {
-		sp[i][0] = par[i];
-	}
-	for (int j = 1; (1 << j) < n; j++) {
-		for (int i = 1; i <= n; i++) {
-			if (sp[i][j - 1] != -1) {
-				sp[i][j] = sp[sp[i][j - 1]][j - 1];
-			}
-		}
-	}
+
+int lca(int u, int v) {
+    if (level[u] > level[v])    swap(u, v);
+    for (int k=K-1; k>=0; k--)
+        if (level[u] + (1<<k) <= level[v])  v = anc[v][k];
+
+    if (u == v) return u;
+    for (int k=K-1; k>=0; k--)
+        if (anc[u][k] != anc[v][k])
+            u = anc[u][k], v = anc[v][k];
+    return anc[u][0];
 }
-int lca(int u, int v) { //finding lca -- n up to 5*10^5
-	if (lev[u] < lev[v]) swap(u, v);
-	for (int j = 19; j >= 0; j--) {
-		if ((lev[u] - (1 << j)) >= lev[v]) {
-			u = sp[u][j];
-		}
-	}
-	if (u == v) return u;
-	for (int j = 19; j >= 0; j--) {
-		if (sp[u][j] != -1 && (sp[u][j] != sp[v][j])) {
-			u = sp[u][j], v = sp[v][j];
-		}
-	}
-	return par[u];
+
+int getanc(int u, int d) {
+    for (int k=0; k<K; k++)
+        if (d & (1<<k))
+            u = anc[u][k];
+    return u;
 }
-int dist(int u, int v) { // finding distance between two node
-	int l = lca(u, v);
-	return lev[u] + lev[v] - 2 * lev[l];
+
+int dist(int u, int v) {
+    int g = lca(u, v);
+    return level[u] + level[v] - 2*level[g];
+}
+
+
+
+///Given u, v, d per query find dth node (0 indexed) on path from u to v.
+///https://codeforces.com/gym/102694/problem/C
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+
+    for (int i=1; i<n; i++) {
+        int u, v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    setup(1, 0);
+
+    int q;
+    cin>>q;
+
+    while (q--) {
+        int u, v, d, ans;
+        cin>>u>>v>>d;
+
+        int g = lca(u, v);
+        int ll = level[u] - level[g];
+        int rr = level[v] - level[g];
+
+        if (d >= ll + rr)   ans = v;
+        else if (d <= ll)   ans = getanc(u, d);
+        else                ans = getanc(v, ll+rr-d);
+        cout<<ans<<"\n";
+    }
 }
